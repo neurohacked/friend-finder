107ed0d932a7579a8813c1211402ba7acae96cd1
neurohacked <admin@neurohacked.com>
2016-09-27 22:25:11 -0700

    Add dummy data
    Add some dummy friend data.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,34 @@
 // friends array
-var friendArray = [
-];
+var friendArray = [{
+    name: "Dade Murphy",
+    photo: "http://i.imgur.com/aiOaDLR.png",
+    scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+    ]
+}, {
+    name: "Kate Libby",
+    photo: "https://i.imgur.com/cq75DST.jpg",
+    scores: [
+        "4",
+        "1",
+        "1",
+        "3",
+        "4",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+    ]
+}];
 
 module.exports = friendArray;
