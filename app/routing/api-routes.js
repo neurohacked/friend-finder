707d159094abf6ade47649f2245e4814b56f7194
neurohacked <admin@neurohacked.com>
2016-09-28 15:43:04 -0700

    Cleanup and fix friend comparison.
    Reduce code that compares submission with existing data.

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -8,55 +8,21 @@ module.exports = function(app) {
     });
     // post method called from form
     app.post('/api/friends', function(request, response) {
-        // pull data from form sent through jquery and push into friends array
+        // // push form data into friends array
         var newFriend = request.body;
         friends.push(newFriend);
-        var count = 0;
-        var bestMatch = [];
+        var matchIndex = 0;
+        var matchDiffs = [];
 
-        function diff() {
+        for (var i = 0; i < friends.length; i++) {
             var totalDiff = 0;
-            // current user scores will be the array index of length - 1
-            var userScores = friends[friends.length - 1].scores;
-            var friendScores = friends[count].scores;
-            // loop until count is equal to the length of the friends array
-            // calculate difference between user and available friends
-            if (count < friends.length - 1) {
-                console.log('Friend Scores:', friendScores);
-                var userScoresArray = [];
-                var friendScoreArray = [];
-                // get string back from the post method so use parse to turn into number
-                // pushes numbers from the survey into an array
-                for (var i = 0; i < userScores.length; i++) {
-                    userScoresArray.push(parseInt(userScores[i]));
-                    friendScoreArray.push(parseInt(friendScores[i]));
-                }
-                console.log('User Scores Array:', userScoresArray);
-                console.log('Friend Scores Array:', friendScoreArray);
-                // subtract each number from the survey based on the array index
-                // Math.abs to get absolute value
-                for (var j = 0; j < userScoresArray.length; j++) {
-                    totalDiff += Math.abs(userScoresArray[j] - friendScoreArray[j]);
-                }
-                console.log('Total Difference:', totalDiff);
-                // push the number to the bestMatch array
-                // if the diff after each iteration is less than the previous number
-                // or if the array is empty
-                if (totalDiff < bestMatch[0] || !bestMatch[0]) {
-                    bestMatch[0] = count;
-                }
-                // increment count so loop can continue through the friends array
-                count++;
-                console.log('Best Match:', bestMatch);
-                diff();
-            } else {
-                // once loop is finished send back the response to the post request
-                console.log('reached the end');
-                console.log(bestMatch);
-                response.json(friends[bestMatch]);
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                totalDiff += Math.abs(request.body.scores[j] - friends[i].scores[j]);
             }
+            matchDiffs.push(totalDiff);
         }
-        diff();
 
+        matchIndex = matchDiffs.indexOf(Math.min.apply(Math, matchDiffs));
+        response.json(friends[matchIndex]);
     });
 };
